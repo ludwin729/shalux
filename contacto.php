e8def32bfb0a937136f011a251a341c35559b353
ludwin729 <ludwintac@gmail.com>
2023-02-09 10:12:53 -0500

    3 version

---
+++ contacto.php
@@ -0,0 +1,143 @@
+<?php
+$pagina = basename(__FILE__);
+//$classBody = "contact-pg";
+$page_title = "Shalux Lighting | Lo mejor en iluminación";
+ob_start();
+?>	
+
+<section>
+			<!-- Breadcrumbs Start -->
+			<div class="rs-breadcrumbs img1">
+			    <div class="container">
+			        <div class="breadcrumbs-inner">
+			            <h1 class="page-title">
+			               Contáctanos
+			            </h1>
+			            <div class="description">
+                    		<p> Con gusto atenderemos tus dudas, recibiremos tus comentarios y te brindaremos el servicio que requieres.<br>
+                            Ponemos a tu alcance el medio de comunicación más cómodo para ti.</p>
+                    	</div>                       
+			        </div>
+			    </div>
+			</div>
+			<!-- Breadcrumbs End -->
+
+			<!-- Contact Section End -->
+			<div class="rs-contact contact-style3 pt-120 pb-120 md-pt-80 md-pb-80">
+			    <div class="container custom2">
+			        <div class="row">
+			            <div class="col-lg-5 md-mb-50">
+			              	<div class="rs-contact-box">
+				              	<div class="sec-title2 mb-40">
+	  								<div class="brush-content mb-20">
+	  									<img src="assets/images/brush.png" alt="">
+	  									<span class="sub-text">Contáctanos</span>
+	  								</div>
+	  								<h2 class="title small pb-15">
+                                      Ponerse en contacto
+	  								</h2>
+	  								<div class="desc">
+
+                                </div>
+	  							</div>
+	  							<div class="address-item mb-30">
+	  								<div class="address-icon">
+	  									<img src="assets/images/contact/style1/1.png" alt="">
+	  								</div>
+	  								<div class="address-content">
+	  									<span class="label">Visítanos</span>
+                                          <a class="des" href="https://goo.gl/maps/Hsp2rWWvgftvwXfv7" target="_blank">
+                                          Av. Guillermo Dansey #351 - Oficina #322
+Cercado de Lima
+                                        </a>
+	  								</div>
+	  							</div>
+	  							<div class="address-item mb-30">
+	  								<div class="address-icon">
+	  									<img src="assets/images/contact/style1/2.png" alt="">
+	  								</div>
+	  								<div class="address-content">
+	  									<span class="label">Envíenos un correo electrónico:</span>
+                                          <a class="des" href="mailto:ventas@shaluxlighting.com">ventas@shaluxlighting.com</a>
+	  								</div>
+	  							</div>
+	  							<div class="address-item">
+	  								<div class="address-icon">
+	  									<img src="assets/images/contact/style1/3.png" alt="">
+	  								</div>
+	  								<div class="address-content">
+	  									<span class="label">Teléfono:</span>
+                                          <a class="des" href="tel:+51946574347">946 574 347</a>
+	  									 <br>
+                                           <a class="des" href="tel:+51954775210">954 775 210</a>
+	  								</div>
+	  							</div>
+			              	</div>
+			            </div>
+			            <div class="col-lg-7 pl-50 md-pl-15">
+			                <div class="contact-drop-line">
+			                	<div class="heading-inner-title mb-30">
+			                		<h2 class="title">Escríbanos</h2>
+			                	</div>
+			                    <div id="form-messages"></div>
+			                    <form id="contact-form" method="post" action="https://devsdesign.net/products/html/clenia/mailer.php">
+			                        <fieldset>
+			                            <div class="row">
+			                                <div class="col-lg-6 mb-30 col-md-6 col-sm-6">
+			                                    <input class="from-control" type="text" id="name" name="name" placeholder="Nombre" required="">
+			                                </div> 
+			                                <div class="col-lg-6 mb-30 col-md-6 col-sm-6">
+			                                    <input class="from-control" type="text" id="email" name="email" placeholder="E-Mail" required="">
+			                                </div>   
+			                                <div class="col-lg-6 mb-30 col-md-6 col-sm-6">
+			                                    <input class="from-control" type="text" id="phone" name="phone" placeholder="Teléfono" required="">
+			                                </div>   
+			                                <div class="col-lg-6 mb-30 col-md-6 col-sm-6">
+			                                    <input class="from-control" type="text" id="phone" name="city" placeholder="Ciudad" required="">
+			                                </div> 
+			                                <div class="col-lg-12 mb-30">
+			                                    <textarea class="from-control" id="message" name="message" placeholder="Su mensaje aquí" required=""></textarea>
+			                                </div>
+			                            </div>
+			                            <div class="btn-part">                                            
+			                               	<div class="form-group mb-0">
+                                                <p class="submit-btn submit-btn3">
+                                                	<input type="submit" value="Enviar Ahora">
+                                                </p>
+                                            </div>
+			                            </div> 
+			                        </fieldset>
+			                    </form> 
+			                </div>
+			            </div>
+			        </div>
+			        <div class="map-canvas pt-120 md-pt-80">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.974468253185!2d-77.04902558326503!3d-12.045277621838023!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c9eb574b2287%3A0xf1f838b12421fae4!2sShalux%20Lighting%20Peru!5e0!3m2!1ses!2spe!4v1675910011386!5m2!1ses!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    			        </div> 
+			    </div> 
+			</div>
+			<!-- Contact Section End -->
+
+
+</section>
+
+
+
+
+
+
+<?php
+$page_content = ob_get_contents();
+ob_end_clean();
+
+//Extras
+ob_start();
+?>
+
+<?php
+$extras=ob_get_contents();
+ob_end_clean();
+
+//Apply the template
+include("template.php");
+?>
---
+++ template.php
@@ -0,0 +1,435 @@
+<?php
+$pagina = isset($pagina) ? $pagina : "";
+//$arreglo_servicios=array("",);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- meta tag -->
+    <meta charset="utf-8">
+    <title> Shalux Lighting | Lo mejor en iluminación</title>
+
+    <meta name="description" content="">
+    <!-- responsive tag -->
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.html">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+    <!-- Bootstrap v4.4.1 css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <!-- font-awesome css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <!-- Uicons Regular Rounded css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/uicons-regular-rounded.css">
+    <!-- flaticon css -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/flaticon.css">
+    <!-- animate css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- owl.carousel css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <!-- off canvas css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/off-canvas.css">
+    <!-- magnific popup css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+    <!-- Main Menu css -->
+    <link rel="stylesheet" href="assets/css/rsmenu-main.css">
+    <!-- nivo slider CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/inc/custom-slider/css/nivo-slider.css">
+    <link rel="stylesheet" type="text/css" href="assets/inc/custom-slider/css/preview.css">
+    <!-- spacing css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/rs-spacing.css">
+    <!-- style css -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- This stylesheet dynamically changed from style.less -->
+    <!-- responsive css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+</head>
+<body class="home-green-color">
+
+<div class="full-width-header">
+			<!--Header Start-->
+			<header id="rs-header" class="rs-header header-style3 header-transparent">
+				<!-- Toolbar Area Start -->
+				<div class="toolbar-area toolber-style3 hidden-md">
+					<div class="container">
+						<div class="row y-middle">
+							<div class="col-lg-8">
+								<ul class="topbar-contact">
+									<li>
+										<i class="fi fi-rr-envelope-plus"></i>
+										<a href="mailto:ventas@shaluxlighting.com">ventas@shaluxlighting.com</a>
+									</li>
+									<li>
+										<i class="fi fi-rr-phone-call"></i>
+										<a class="no-border" href="tel:+51946574347">946 574 347</a>
+									</li>
+									<li>
+										<a href="https://goo.gl/maps/Hsp2rWWvgftvwXfv7" target="_blank">
+											<i class="fi fi-rr-map-marker-home"></i>
+											Av. Guillermo Dansey Nº 351- Tda 322</a>
+									</li>
+								</ul>
+							</div>
+							<div class="col-lg-4 text-right">
+								<div class="toolbar-sl-share">
+									<ul>
+										<li class="opening"> <em><i class="fi fi-rr-time-add"></i> 08:00 - 20:00</em>
+										</li>
+										<li><a href="https://www.facebook.com/ShaLuxLighting/"><i
+													class="fa fa-facebook"></i></a></li>
+										<li><a href="https://www.instagram.com/shaluxlighting/"><i
+													class="fa fa-instagram"></i></a></li>
+										<li><a href="https://wa.me/+51946574347" target="_blank"><i
+													class="fa fa-whatsapp"></i></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Toolbar Area End -->
+
+				<!-- Menu Start -->
+				<div class="menu-area menu-sticky">
+					<div class="container">
+						<div class="row-table">
+							<div class="col-cell header-logo">
+								<div class="logo-area">
+									<a href="index.php">
+										<img class="normal-logo" src="assets/images/shalux.png" alt="logo">
+										<img class="sticky-logo" src="assets/images/shalux.png" alt="logo">
+									</a>
+								</div>
+							</div>
+							<div class="col-cell">
+								<div class="rs-menu-area">
+									<div class="main-menu">
+										<nav class="rs-menu hidden-md">
+											<ul class="nav-menu">
+												<li class="menu-item-has-children current-menu-item">
+													<a href="index.php">Inicio</a>
+
+												</li>
+												<li class="menu-item-has-children">
+													<a href="nosotros.php">Nosotros</a>
+												</li>
+												<li class="menu-item-has-children">
+													<a href="#">Productos</a>
+													<ul class="sub-menu">
+														<li><a href="luminarias_de_interior.php">Luminarias de Interior</a></li>
+														<li><a href="luminarias_de_exterior.php">Luminarias de Exterior</a></li>
+                                                        <li><a href="luminarias_acuaticas.php" >Luminarias Acuaticas</a></li>
+                                                        <li><a href="luminarias_solares.php">Luminarias Solares</a></li>
+                                                        <li><a href="luminarias_industriales.php">Luminarias Industriales</a></li>
+													</ul>
+												</li>
+												<li class="menu-item-has-children">
+													<a href="clientes.php">Clientes</a>
+													<ul class="sub-menu">
+														<li><a href="projects.html">Projects</a></li>
+														<li><a href="projects-details.html">Projects Details</a></li>
+													</ul>
+												</li>
+												<li class="menu-item-has-children">
+													<a href="blog.php">Blog</a>
+													<ul class="sub-menu">
+														<li><a href="blog.php">Blog</a></li>
+														<li><a href="single-post.html">Single Post</a></li>
+													</ul>
+												</li>
+												<li>
+													<a href="contacto.php">Contacto</a>
+												</li>
+											</ul> <!-- //.nav-menu -->
+										</nav>
+									</div> <!-- //.main-menu -->
+								</div>
+							</div>
+							<div class="col-cell">
+								<div class="expand-btn-inner">
+									<ul>
+										<li class="btn-quote"><a href="#">🛒 Tienda Virtual</a></li>
+										<li class="nav-link">
+											<a id="nav-expander" class="nav-expander bar" href="#">
+												<span class="dot-hum"></span>
+												<span class="dot-hum"></span>
+												<span class="dot-hum"></span>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Menu End -->
+
+				<!-- Canvas Mobile Menu start -->
+				<nav class="right_menu_togle mobile-navbar-menu" id="mobile-navbar-menu">
+					<div class="close-btn">
+						<a id="nav-close2" class="nav-close">
+							<div class="line">
+								<span class="line1"></span>
+								<span class="line2"></span>
+							</div>
+						</a>
+					</div>
+					<ul class="nav-menu">
+						<li class="menu-item-has-children current-menu-item">
+							<a href="index3.html">Inicio</a>
+							<ul class="sub-menu">
+								<li class="menu-item-has-children current-menu-item">
+									<a href="#">Multipages</a>
+									<ul class="sub-menu">
+										<li><a href="index-2.html">Home One</a></li>
+										<li><a href="index2.html">Home Two</a></li>
+										<li class="active"><a href="index3.html">Home Three</a></li>
+										<li><a href="index4.html">Home Four</a></li>
+										<li><a href="index5.html">Home Five</a></li>
+									</ul>
+								</li>
+								<li class="menu-item-has-children">
+									<a href="#">Onepages</a>
+									<ul class="sub-menu">
+										<li><a href="onepage1.html">Onepage One</a></li>
+										<li><a href="onepage2.html">Onepage Two</a></li>
+										<li><a href="onepage3.html">Onepage Three</a></li>
+										<li><a href="onepage4.html">Onepage Four</a></li>
+										<li><a href="onepage5.html">Onepage Five</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li class="menu-item-has-children">
+							<a href="#">Nosotros</a>
+							<ul class="sub-menu">
+								<li><a href="nosotros.php">About</a></li>
+								<li><a href="faqs.html">Faqs</a></li>
+								<li><a href="request-an-estimate.html">Reguest An Estimate</a></li>
+								<li><a href="expert-members.html">Expert Members</a></li>
+								<li><a href="single-member.html">Single Member</a></li>
+							</ul>
+						</li>
+						<li class="menu-item-has-children">
+							<a href="#">Productos</a>
+							<ul class="sub-menu">
+								<li><a href="services1.html">Services 01</a></li>
+								<li><a href="services2.html">Services 02</a></li>
+								<li class="last-item menu-item-has-children">
+									<a href="#">Services Details</a>
+									<ul class="sub-menu">
+										<li><a href="covid-19-cleaning.html">Covid-19 Cleaning</a></li>
+										<li><a href="kitchen-cleaning.html">Kitchen Cleaning</a> </li>
+										<li><a href="toilet-cleaning.html">Toilet Cleaning</a></li>
+										<li><a href="office-cleaning.html">Office Cleaning</a></li>
+										<li><a href="window-cleaning.html">OFICINAS</a></li>
+										<li><a href="carpet-cleaning.html">CANCHAS DEPORTIVAS</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li class="menu-item-has-children">
+							<a href="clientes.php">Clientes</a>
+							<ul class="sub-menu">
+								<li><a href="projects.html">Projects</a></li>
+								<li><a href="projects-details.html">Projects Details</a></li>
+							</ul>
+						</li>
+						<li class="menu-item-has-children">
+							<a href="#">Blog</a>
+							<ul class="sub-menu">
+								<li><a href="blog.php">Blog</a></li>
+								<li><a href="single-post.html">Single Post</a></li>
+							</ul>
+						</li>
+						<li>
+							<a href="contacto.php">Contacto</a>
+						</li>
+					</ul> <!-- //.nav-menu -->
+					<div class="canvas-contact">
+						<div class="address-area">
+							<div class="address-list">
+								<div class="info-icon">
+									<i class="flaticon-location"></i>
+								</div>
+								<div class="info-content">
+									<h4 class="title">Address</h4>
+									<em>05 kandi BR. New York</em>
+								</div>
+							</div>
+							<div class="address-list">
+								<div class="info-icon">
+									<i class="flaticon-email"></i>
+								</div>
+								<div class="info-content">
+									<h4 class="title">Email</h4>
+									<em><a href="mailto:support@reactheme.com">support@reactheme.com</a></em>
+								</div>
+							</div>
+							<div class="address-list">
+								<div class="info-icon">
+									<i class="flaticon-call"></i>
+								</div>
+								<div class="info-content">
+									<h4 class="title">Phone</h4>
+									<em>+019988772</em>
+								</div>
+							</div>
+						</div>
+					</div>
+				</nav>
+				<!-- Canvas Menu end -->
+			</header>
+			<!--Header End-->
+		</div>
+		<!--Full width header End-->
+
+<?php echo $page_content; ?>
+	<!-- Footer Start -->
+	<footer id="rs-footer" class="rs-footer footer-style1 footer-style2">
+		<div class="footer-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8">
+						<div class="row">
+							<div class="col-lg-6 col-md-12 col-sm-12 md-mb-20 pr-83 md-pr-15">
+								<div class="footer-logo mb-30 md-mb-20">
+									<a href="index.php"><img src="assets/images/shalux.png" alt=""></a>
+								</div>
+								<div class="textwidget mb-33">
+									Somos una empresa importadora y comercializadora de productos de iluminación LED,
+									buscamos entregar a nuestros clientes soluciones concretas e integrales al momento
+									de iluminar sus espacios y proyectos. </div>
+							</div>
+							<div class="col-lg-6 col-md-12 col-sm-12 md-mb-20">
+								<h3 class="footer-title">Información de Contacto</h3>
+								<ul class="address-widget">
+									<li>
+										<i class="fi fi-rr-map-marker-home"></i>
+										<div class="desc"> <a href="https://goo.gl/maps/Hsp2rWWvgftvwXfv7"
+												target="_blank">Av. Guillermo Dansey Nº 351- Tda 322<br>
+												Cercado de Lima</a>
+										</div>
+									</li>
+									<li>
+										<i class="fi fi-rr-phone-call"></i>
+										<div class="desc">
+											<a class="no-border" href="tel:+51900461720">900 461 720</a>
+										</div>
+									</li>
+									<li>
+										<i class="fi fi-rr-envelope-plus"></i>
+										<div class="desc">
+											<a href="mailto:ventas@shaluxlighting.com">ventas@shaluxlighting.com</a>
+										</div>
+									</li>
+									<li>
+										<i class="fi fi-rr-time-add"></i>
+										<div class="desc">
+											08:00am-8:00pm
+										</div>
+									</li>
+									<ul class="footer-social">
+										<li><a href="https://www.facebook.com/ShaLuxLighting/"><i
+													class="fa fa-facebook"></i></a></li>
+										<li><a href="https://www.instagram.com/shaluxlighting/"><i
+													class="fa fa-instagram"></i></a></li>
+										<li><a href="https://wa.me/+51946574347" target="_blank"><i
+													class="fa fa-whatsapp"></i></a></li>
+									</ul>
+								</ul>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="col-lg-4 col-md-12 col-sm-12 md-mb-10 pl-90 md-pl-15">
+						<h3 class="footer-title">SUSCRÍBETE A NUESTRO BOLETÍN</h3>
+						<p class="widget-desc white-color">Únase a nuestra lista de suscriptores para obtener las
+							últimas noticias y ofertas especiales</p>
+						<p>
+							<input type="email" name="email" placeholder="Su dirección de correo electrónico"
+								required="">
+							<input type="submit" value="Suscríbete Ahora">
+						</p>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row y-middle">
+					<div class="col-lg-6 md-mb-10 text-lg-end text-center order-last">
+						<ul class="copy-right-menu">
+							<li><a href="index-2.html">Inicio</a></li>
+							<li><a href="nosotros.php">Nosotros</a></li>
+							<li><a href="productos.php">Productos</a></li>
+							<li><a href="blog.php">Blog</a></li>
+							<li><a href="contacto.php">Contacto</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-6">
+						<div class="copyright text-lg-start text-center ">
+							<p>Copyright <?php echo date('Y'); ?> &copy; Shalux Lighting. Diseñada por <a href="#" 
+									target="blank">LR Digital Solutions</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- Footer End -->
+
+	<!-- start scrollUp  -->
+	<div id="scrollUp" class="blue-color">
+		<i class="fi fi-rr-arrow-small-up"></i>
+	</div>
+	<!-- End scrollUp  -->
+
+
+
+	<!-- modernizr js -->
+	<script src="assets/js/modernizr-2.8.3.min.js"></script>
+	<!-- jquery latest version -->
+	<script src="assets/js/jquery.min.js"></script>
+	<!-- Bootstrap v4.4.1 js -->
+	<script src="assets/js/bootstrap.min.js"></script>
+	<!-- op nav js -->
+	<script src="assets/js/jquery.nav.js"></script>
+	<!-- owl.carousel js -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!-- wow js -->
+	<script src="assets/js/wow.min.js"></script>
+	<!-- Skill bar js -->
+	<script src="assets/js/skill.bars.jquery.js"></script>
+	<!-- imagesloaded js -->
+	<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+	<!-- waypoints.min js -->
+	<script src="assets/js/waypoints.min.js"></script>
+	<!-- counterup.min js -->
+	<script src="assets/js/jquery.counterup.min.js"></script>
+	<!-- magnific popup js -->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/jquery.easypiechart.min.js"></script>
+	<!-- Nivo slider js -->
+	<script src="assets/inc/custom-slider/js/jquery.nivo.slider.js"></script>
+	<!-- contact form js -->
+	<script src="assets/js/contact.form.js"></script>
+	<!-- main js -->
+	<script src="assets/js/main.js"></script>
+
+
+</html>
+<body>
+
+
+
+<?php echo isset($extras)?$extras:""; ?>
+
+</php>
